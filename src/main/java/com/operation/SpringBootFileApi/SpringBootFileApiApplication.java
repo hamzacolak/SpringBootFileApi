5fcfd0d19adc0f49ef02106b7518b6f4594d38a4
hamzacolak <hamza.colak@192.168.1.102>
2021-11-21 23:08:39 +0300

    swagger configuration

--- src/main/java/com/operation/SpringBootFileApi/SpringBootFileApiApplication.java
+++ src/main/java/com/operation/SpringBootFileApi/SpringBootFileApiApplication.java
@@ -2,8 +2,13 @@ package com.operation.SpringBootFileApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example")
+@EnableSwagger2
 public class SpringBootFileApiApplication {
 
 	public static void main(String[] args) {
