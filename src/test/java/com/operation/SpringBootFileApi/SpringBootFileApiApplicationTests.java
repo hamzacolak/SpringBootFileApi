1bb5d6cc113ae9e5853a7048bef10660f7a3767b
Hamza COLAK <hamsacolak@gmail.com>
2021-11-21 22:12:47 +0300

    first commit
    project created.

---
+++ src/test/java/com/operation/SpringBootFileApi/SpringBootFileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operation.SpringBootFileApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
